5bbc03eb6a46e80686628ba99110db717c0cdc2c
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-20 19:39:20 +0530

    Resolved all conflicts between feature and dev branches

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -103,19 +103,6 @@ local function plugins(use)
     requires = "nvim-lua/plenary.nvim"
   }
 
-  --> Statusline
-  use { "nvim-lualine/lualine.nvim",
-  requires = { "kyazdani42/nvim-web-devicons", opt = true } }
-
-  --> Persistant floating terminals that can be toggled
-  use { "akinsho/toggleterm.nvim", tag = "v2.*" }
-
-  --> Git support
-  use { "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" }
-
-  --> Show levels of indentation
-  use "lukas-reineke/indent-blankline.nvim"
-
   --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   -- PARSE PROGRAMMING LANGUAGE SOURCE CODE
   --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
