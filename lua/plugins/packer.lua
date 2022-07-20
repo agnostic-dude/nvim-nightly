07ba1684c0806739f4023a89d2d89af17137cc8c
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-20 16:24:35 +0530

    Updated init file after a break

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -103,6 +103,28 @@ local function plugins(use)
     requires = "nvim-lua/plenary.nvim"
   }
 
+<<<<<<< Updated upstream
+=======
+  --> Statusline
+  use { "nvim-lualine/lualine.nvim",
+  requires = { "kyazdani42/nvim-web-devicons", opt = true } }
+
+  --> Persistant floating terminals that can be toggled
+  use { "akinsho/toggleterm.nvim", tag = "v2.*" }
+
+  --> Git support
+  use { "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" }
+
+  --> Show levels of indentation
+  use "lukas-reineke/indent-blankline.nvim"
+
+  --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  -- PARSE PROGRAMMING LANGUAGE SOURCE CODE
+  --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  --
+  --
+
+>>>>>>> Stashed changes
   if packer_bootstrap then
     print "Need to restart neovim after installation!"
     require("packer").sync()
