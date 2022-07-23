e31987e937826efe483f90fddd89e3e2a6253f15
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-24 00:27:19 +0530

    Added plugins required for autocompletion including sources & snippets

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -95,8 +95,12 @@ local function plugins(use)
   use { "nvim-treesitter/nvim-treesitter", run = "<Cmd>TSUpdate" }
   use "nvim-treesitter/nvim-treesitter-textobjects"
 
-  --> Configurations for builtin LSP client
-  use "neovim/nvim-lspconfig"
+
+  use "neovim/nvim-lspconfig" --> Configurations for builtin LSP client
+  use "hrsh7th/nvim-cmp"      --> Autocompletion plugin
+  use "hrsh7th/cmp-nvim-lsp"  --> LSP source for nvim-cmp
+  use "saadparwaiz1/cmp_luasnip" --> Snippets source for nvim-cmp
+  use "L3MON4D3/LuaSnip"      --> Snippets plugin
 
   --> Find, filter, preview & pick
   use { "nvim-telescope/telescope.nvim", branch = "0.1.x",
