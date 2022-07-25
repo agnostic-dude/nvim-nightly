105ae21cbab94551e8b5aea6f1f79466feef1f85
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-25 21:49:54 +0530

    Single press of <Escape> now leaves telescope view

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,8 @@
 -- Neovim Nightly Edtion (version 0.8)
 -- Telescope configuration
 --=============================================================================
+local actions = require "telescope.actions"
+
 require("telescope").setup {
   defaults = {
     mappings = {
@@ -9,6 +11,10 @@ require("telescope").setup {
         -- map actions.which_key to to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker
         ["<C-h>"] = "which_key",
+
+        -- By default telescope enters NORMAL mode on <Esc>, you have to press
+        -- <Esc> twice to exit; following makes telescope exit on <Esc>
+        ["<Esc>"] = actions.close,
       }
     }
   },
