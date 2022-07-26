6be0a004d691f3d4a297255d5e34f6d1c2c5bba8
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-26 23:56:00 +0530

    Changed diagnostic sign for warning

--- lua/plugins/lsp/launcher.lua
+++ lua/plugins/lsp/launcher.lua
@@ -46,7 +46,7 @@ end
 -------------------------------------------------------------------------------
 -- Configure LSP diagnostics
 -------------------------------------------------------------------------------
-local signs = { Error = "", Warn = "", Info = "", Hint = "" }
+local signs = { Error = "", Warn = "⚠", Info = "", Hint = "" } -- warn =""
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
