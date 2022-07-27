d4ee4848a7ab52ff80216f625c695e2ea38417be
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-07-27 14:45:21 +0530

    Autocomamnd created for document fromatting on file save (augroup = FormatOnSave)

--- lua/plugins/lsp/autocommands.lua
+++ lua/plugins/lsp/autocommands.lua
@@ -1,11 +1,26 @@
--- require "plugins.lsp.code_action"
+--=============================================================================
+-- Neovim Nightly Edtion (version 0.8)
+-- LSP configuration: keymappings
+--=============================================================================
+-- Show code actions when available
+local code_action_utils = require "plugins.lsp.code_action"
 
-local code_action_utils = require("plugins.lsp.code_action")
-local code_action = vim.api.nvim_create_augroup("CodeAction", { clear = true })
 vim.api.nvim_create_autocmd(
   { "CursorHold", "CursorHoldI" }, {
   pattern = "*",
-  callback = code_action_utils.code_action_listener,
-  group = code_action,
-}
-)
+  callback = function ()
+    code_action_utils.code_action_listener()
+  end,
+  group = vim.api.nvim_create_augroup("ShowCodeAction", { clear = true }),
+})
+
+-- Format file on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+  group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+})
+
+-- vim.cmd [[ autocmd BufWritePre <buffer> lua vim.lsp.buf.format() ]]
