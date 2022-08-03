47beca157d90ab1cd9a0062d4b88fdf5d3b40402
agnostic-dude <agnosticismakesworldbetter@gmail.com>
2022-08-03 20:10:11 +0530

    Prioritize most severe diagnostic signs in the sign column (aka gutter)

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -8,3 +8,25 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- Most severe DiagnosticSign is shown in gutter per line
+-- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#customize-lsp-codelens-and-signs
+vim.diagnostic.config {
+  virtual_text = {
+    source = "always",
+    prefix = "■",
+    -- Only show virtual text matching the given severity
+    severity = {
+      -- Specify a range of severities
+      min = vim.diagnostic.severity.ERROR,
+    },
+  },
+  float = {
+    source = "always",
+    border = "rounded",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+}
